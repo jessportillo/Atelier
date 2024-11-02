1808880dda518b0f5b4007257377b6852f3bd896
S Dx <sdxsofteng@gmail.com>
2024-11-02 18:20:00 -0400

    ADD - AuditaData class

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+from datetime import datetime
+from models.audita.audita_data import AuditaData
+
 def main():
+    # Test dates
+    date_1: datetime = datetime(2023, 10, 1, 12, 0)
+    date_2: datetime = datetime(2023, 10, 1, 14, 30)
+    
+    # Audita Data Creation
+    audita_data_1: AuditaData = AuditaData(date_1, date_2)
+
+    # Test for audita_data_creation
+    print(f"Audita Data Total Time: {audita_data_1.getTotalTimeInMinutes()} minutes")
+
     print("Program!")
 
 if __name__ == "__main__":
---
+++ models/audita/audita_data.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timedelta
+import math
+
+class AuditaData:
+    def __init__(self, start_date: datetime, end_date: datetime):
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def getTotalTimeInMinutes(self):
+        duration: timedelta = self.end_date - self.start_date
+        total_minutes = duration.total_seconds() / 60.0
+        return math.ceil(total_minutes)
