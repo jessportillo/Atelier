08de3247d42585334a2ec5913b408ea63b16dee4
S Dx <sdxsofteng@gmail.com>
2024-11-03 20:32:55 -0500

    ADD - Cleanup

--- main.py
+++ main.py
@@ -13,43 +13,12 @@ from PyQt5.QtWidgets import (
 from typing import List
 
 def main():
-    # # Test dates
-    # date_1: datetime = datetime(2023, 10, 1, 12, 30)
-    # date_2: datetime = datetime(2023, 10, 1, 14, 30)
-    # date_3: datetime = datetime(2023, 10, 1, 15, 30)
-
-    # # Filter test dates
-    # min_date_1: datetime = datetime(2023, 10, 1, 12, 30)
-    # max_date_2: datetime = date_3
-    
-    # # Audita Data Creation
-    # audita_data_1: AuditaData = AuditaData(date_1, date_2)
-    # audita_data_2: AuditaData = AuditaData(date_2, date_3)
-    # audita_data_3: AuditaData = AuditaData(date_1, date_3)
-
-    # audita_data_list = [audita_data_1, audita_data_2, audita_data_3]
-    # audita_data_group_filter_1: AuditaGroupFilter = AuditaGroupFilter(min_date_1, max_date_2)
-
-    # audita_data_group = AuditaDataGroup(audita_data_list, audita_data_group_filter_1)
-
-    # # Test for audita_data_creation
-    # print(f"Audita Data Total Time: {audita_data_1.getTotalTimeInMinutes()} minutes")
-    # print(audita_data_1)
-    # print(audita_data_group_filter_1)
-    # print(audita_data_group)
-    # print(audita_data_group.getTotalTimeInMinutes())
-    # print("Program!")
     app = QApplication(sys.argv)
     window = GUI(program_loop)
     window.show()
     sys.exit(app.exec_())
 
 def program_loop(selected_folders: List[str], min_date: datetime, max_date: datetime):
-    print("Program Loop !")
-    print(f"Selected folders {selected_folders}")
-    print(f"Min date {min_date}")
-    print(f"Max date {max_date}")
-
     audita_file_paths: List[str] = get_audita_files_paths(selected_folders)
     audita_data_list: List[AuditaData] = interpret_audita_files(audita_file_paths)
     audita_data_group : AuditaDataGroup = filter_audita_data_list(audita_data_list, min_date, max_date)
